a149e801003c6d7a1e75463db20e8f827e52ece6
Falcon Dai <me@falcondai.com>
2016-09-21 21:03:51 -0500

    add IK execution error and summary

--- execute.py
+++ execute.py
@@ -72,10 +72,16 @@ rs = baxter_interface.RobotEnable(baxter_interface.CHECK_VERSION)
 rs.enable()
 
 # task setup
+n_attempts = int(sys.argv[1])
+execution_error_threshold = 0.04
 # z_initial = -0.05
-z_initial = 0.05
+z_initial = 0.
 z_table = -0.165
-camera_pose = {'position': Limb.Point(x=0.7715770154184426, y=-0.3133978759217212, z=0.4350424834433179), 'orientation': Limb.Quaternion(x=-0.5303342084664359, y=0.6182759231154774, z=0.34040073425704037, w=0.469689099664077)}
+grasp_force_threshold = 20.
+# camera_pose = {'position': Limb.Point(x=0.7715770154184426, y=-0.3133978759217212, z=0.4350424834433179), 'orientation': Limb.Quaternion(x=-0.5303342084664359, y=0.6182759231154774, z=0.34040073425704037, w=0.469689099664077)}
+# XYZ -> Left Anterior Superior
+workspace_low = np.asarray([0.62, 0.62])
+workspace_high = np.asarray([0.72, 0.72])
 
 # set up camera
 msg = rospy.wait_for_message('/cameras/left_hand_camera/camera_info', CameraInfo)
@@ -107,30 +113,31 @@ savefig('mask.png')
 # gripper_mask = np.ones((400, 640))
 # gripper_mask[:200, :] = 0
 
-global ox, oy
 def update_image(msg):
     global current_image, current_image_stamp
     current_image = PIL_Image.frombytes('RGBA', (msg.width, msg.height), msg.data)
     current_image_stamp = msg.header.stamp
 rospy.Subscriber('/cameras/left_hand_camera/image', Image, update_image)
 
-# XYZ -> Left Anterior Superior
-workspace_low = np.asarray([0.62, 0.62, -0.02])
-workspace_high = np.asarray([0.72, 0.72, -0.02])
+global force
+def update_gripper_state(msg):
+    global force
+    force = msg.force
+rospy.Subscriber('/robot/end_effector/left_gripper/state', EndEffectorState, update_gripper_state)
 
 # joint limits
-joint_limits = {
-    'left_w0': [0.28838838812245776, -1.8223691760078715],
-    'left_w1': [1.5826846779007757, 0.35051461003181705],
-    'left_w2': [3.056456719861687, -0.062126221909359257],
-    'left_e0': [0.6289321230330196, -0.2826359601678875],
-    'left_e1': [1.5512380717491248, 0.35089810522878839],
-    'left_s0': [-0.21015536794030168, -1.1075341288532687],
-    'left_s1': [-0.27458256103148904, -0.5],
-}
-joint_names = joint_limits.keys()
-joint_low = [a[1] for a in joint_limits.itervalues()]
-joint_high = [a[0] for a in joint_limits.itervalues()]
+# joint_limits = {
+#     'left_w0': [0.28838838812245776, -1.8223691760078715],
+#     'left_w1': [1.5826846779007757, 0.35051461003181705],
+#     'left_w2': [3.056456719861687, -0.062126221909359257],
+#     'left_e0': [0.6289321230330196, -0.2826359601678875],
+#     'left_e1': [1.5512380717491248, 0.35089810522878839],
+#     'left_s0': [-0.21015536794030168, -1.1075341288532687],
+#     'left_s1': [-0.27458256103148904, -0.5],
+# }
+# joint_names = joint_limits.keys()
+# joint_low = [a[1] for a in joint_limits.itervalues()]
+# joint_high = [a[0] for a in joint_limits.itervalues()]
 
 # delineate the workspace boundary
 # for a in xrange(2):
@@ -140,16 +147,21 @@ joint_high = [a[0] for a in joint_limits.itervalues()]
 #         execute_linear(left, x, y, workspace_high[2], Quat.from_v_theta([0,0,1], 0.), n_steps=1)
 # rospy.sleep(1.)
 
-a, b = np.random.rand(2)
-x = a * workspace_low[0] + (1. - a) * workspace_high[0]
-y = b * workspace_low[0] + (1. - b) * workspace_high[0]
-# execute_linear(left, x, y, workspace_high[2], Quat.from_v_theta([0,0,1], np.random.rand() * 2. * np.pi), n_steps=1)
-execute_linear(left, 0.6, 0.7, z_initial, Quat.from_v_theta([0,0,1], 0.), n_steps=1)
+# execute_linear(left, 0.6, 0.7, z_initial, Quat.from_v_theta([0,0,1], 0.), n_steps=1)
 
 # fitting a gaussian
 dbscan = cluster.DBSCAN(15.)
 meanshift = cluster.MeanShift(cluster_all=False)
-for i in xrange(int(sys.argv[1])):
+n_successes = 0
+n_ik_errors = 0
+for i in xrange(n_attempts):
+    print '* attempt %i' % i
+    # move to a random location in workspace
+    a, b, c = np.random.rand(3)
+    x = a * workspace_low[0] + (1. - a) * workspace_high[0]
+    y = b * workspace_low[0] + (1. - b) * workspace_high[0]
+    execute_linear(left, x, y, z_initial, Quat.from_v_theta([0,0,1], c * 2. * np.pi), n_steps=1)
+
     rospy.sleep(1.)
     # localize one cube
     # cu, cv = localize_cm_method(current_image, gripper_mask)
@@ -172,7 +184,7 @@ for i in xrange(int(sys.argv[1])):
     k = (z_table - camera_xyz[2]) / (pt.point.z - camera_xyz[2])
     # intersection: k * pt.point.x, k * pt.point.y, k * pt.point.z
     ox, oy = k * (pt.point.x - camera_xyz[0]) + camera_xyz[0], k * (pt.point.y - camera_xyz[1]) + camera_xyz[1]
-    print ox, oy
+    print 'object location', ox, oy
 
     # u, v = camera.project3dToPixel((ox, oy, z_table))
     # u, v = camera.project3dToPixel((nx, ny, nz))
@@ -201,10 +213,43 @@ for i in xrange(int(sys.argv[1])):
     )
     pub.publish(msg)
 
+    # choose a random
     quat = Quat.from_v_theta([0., 0., 1.], np.random.rand() * 2. * np.pi)
+    # move to pre-grasp pose
     execute_linear(left, ox, oy, z_initial, quat, n_steps=1)
-    execute_planar_grasp(left, left_gripper, z_initial, z_table, n_steps=10)
-    execute_linear(left, 0.6, 0.7, z_initial, Quat.from_v_theta([0,0,1], np.random.rand() * 2. * np.pi), n_steps=1)
+    # IK execution reached pose from the commanded pose
+    execution_error = np.linalg.norm(np.asarray(left.endpoint_pose()['position'])[:2] - [ox, oy])
+    print 'execution error:', execution_error
+    if execution_error < execution_error_threshold:
+        # execute vertical pinch grasp
+        traj = execute_vertical(left, z_table, n_steps=8, duration=4., timeout=8.)
+        left_gripper.close()
+        rospy.sleep(0.5)
+        # lift arm
+        execute_vertical(left, z_initial, n_steps=1, duration=2., timeout=4.)
+        rospy.sleep(0.5)
+        # use grasp force to recognize grasp success
+        print 'grasp force', force
+        if force > grasp_force_threshold:
+            print 'grasp succeeded'
+            # move to a random location in workspace
+            a, b, c = np.random.rand(3)
+            x = a * workspace_low[0] + (1. - a) * workspace_high[0]
+            y = b * workspace_low[0] + (1. - b) * workspace_high[0]
+            execute_linear(left, x, y, z_initial, Quat.from_v_theta([0,0,1], c * 2. * np.pi), n_steps=1, duration=2., timeout=4.)
+            n_successes += 1
+        else:
+            print 'grasp failed'
+        # release object
+        left_gripper.open()
+    else:
+        print 'IK trajectory execution error'
+        n_ik_errors += 1
+        continue
 
 
-# rospy.spin()
+print '* summary'
+print '# of attempts', n_attempts
+print '# of successes', n_successes
+print '# of failures', n_attempts - n_successes
+print '# of IK errors', n_ik_errors
